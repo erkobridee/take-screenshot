9d8fd99e2e8e12b3932435091fa06f5ffb60083e
Erko Bridee <erko.bridee@gmail.com>
2015-03-19 11:14:35 -0300

    server config updated

--- server/config.js
+++ server/config.js
@@ -1,22 +1,52 @@
-var path = require('path');
+var args = require('yargs').argv,
+    path = require('path'),
+    is   = require('../phantomjs/helpers/is');
 
 module.exports = (function() {
 
   var config = {};
 
-  var rootPath = path.dirname(require.main.filename);
-  config.rootPath = rootPath;
+  // config.args = args;
 
-  config.phantomjs = {
-    root: path.resolve( rootPath, '..', 'phantomjs' ),
-    screenshot: path.resolve( rootPath, '..', 'phantomjs', 'screenshot.js' )
+  config.flags = {
+    proxy: !!args.proxy
   };
 
+  config.rootPath = path.dirname(require.main.filename);
+
+  config.phantomjs = {
+    root: path.resolve( config.rootPath, '..', 'phantomjs' ),
+    screenshot: path.resolve( config.rootPath, '..', 'phantomjs', 'screenshot.js' )
+  };
+
+  if( config.flags.proxy ) {
+
+    var proxy = {
+      server : (
+        ( is.string( args.proxy ) ) ?
+          args.proxy :
+          process.env.PROXY_SERVER
+      ),
+      auth   : (
+        ( args.proxyAuth && is.string( args.proxyAuth ) ) ?
+          args.proxyAuth :
+          process.env.PROXY_AUTH
+      )
+    }
+
+    //---
+    config.phantomjs.proxy = proxy;
+  }
+
   config.server = {
-    port: 9000,
-    public_dir: path.resolve( rootPath, 'public' ),
-    index_html: path.resolve( rootPath, 'public', 'index.html' ),
-    screenshots: path.resolve( rootPath, 'files' )
+    port: (
+      ( args.port && is.string.number( args.port ) ) ?
+        args.port :
+        9000
+    ),
+    public_dir: path.resolve( config.rootPath, 'public' ),
+    index_html: path.resolve( config.rootPath, 'public', 'index.html' ),
+    screenshots: path.resolve( config.rootPath, 'files' )
   };
 
   return config;
