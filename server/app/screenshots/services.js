f85ac2129069de675561ef03a399bc994fd4fbf6
Erko Bridee <erko.bridee@gmail.com>
2015-03-24 19:33:07 -0300

    server/screenshot/service reviewed

--- server/app/screenshots/services.js
+++ server/app/screenshots/services.js
@@ -24,7 +24,7 @@ module.exports = (function() {
         });
 
       } else {
-        return cb( 'screenshot already exists' );
+        return cb( 'screenshot ' + screenshotFor.id + ' already exists' );
       }
     });
   }
